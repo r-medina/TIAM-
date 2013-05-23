16ab6e7bb5d39d23800b9732f4aeea55c77e8a27
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-05-23 00:22:38 -0400

    session.py: pickle Y_hmm and Y_svm
    (only aligned with Y when no time series are rejected)

--- classifyTracks/src/session.py
+++ classifyTracks/src/session.py
@@ -9,14 +9,14 @@ X = pickle.load(open('../out/X.pk', 'r'))
 Y = pickle.load(open('../out/Y.pk', 'r'))
 
 # clip range for each feature
-clip = {'straightness': [-0.5, 1.0],
+clip = {'straightness': [-1.0, 1.0],
         'bending': [-0.5, 0.5],
-        'efficiency': [0., 1.], 
+        'efficiency': [0., 0.2], 
         'asymmetry': [0., 1.],
         'skewness': [-2., 2.],
         'kurtosis': [1., 5.],
-        'displacement': [0., 15.],
-        'confinement': [-12, 0.]}
+        'displacement': [0., 20.],
+        'confinement': [-2, 2.]}
 for feat in clip:
     X[feat][X[feat]<clip[feat][0]] = clip[feat][0]
     X[feat][X[feat]>clip[feat][1]] = clip[feat][1]
@@ -36,6 +36,8 @@ for n, track in D.groupby(level=0):
     if any(msk):
         xs.append(x[y >= 0])
         ys.append(y[y >= 0])
+    else:
+        print n 
 
 # set up hmm
 model = hmm.GaussianHMM(
@@ -50,6 +52,8 @@ model = hmm.GaussianHMM(
 model.fit(xs)
 # get states. note: this is a list, containing all tracks where y!=-1
 y_hmm = [model.decode(x)[1] for x in xs]
+Y_hmm = pd.DataFrame(np.concatenate(y_hmm)[:,None],index=Y.index, columns=['labels'])
+pickle.dump(Y_hmm, open('Y_hmm.pk', 'w'))
 print y_hmm
 
 # run SVM analysis
@@ -61,5 +65,6 @@ svc = svm.SVC()
 performance = cross_validation.cross_val_score(svc, x_scaled, y, cv=kfold, n_jobs=-1)
 svc.fit(x_scaled, y)
 # note: this is length 8800, all tracks concatenated
-y_svc = svc.predict(x_scaled)
-print y_svc
+y_svm = svc.predict(x_scaled)
+Y_svm = pd.DataFrame(y_svm[:,None],index=Y.index, columns=['labels'])
+pickle.dump(Y_svm, open('Y_svm.pk', 'w'))
