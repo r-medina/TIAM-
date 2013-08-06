5baa66214c18276444681b6f0f612441e15f7a21
Ricardo Medina <ricky.medina91@gmail.com>
2013-08-06 16:01:02 -0400

    jank ass arrest coeff code

--- classifyTracks/src/arrest_coeff.py
+++ classifyTracks/src/arrest_coeff.py
@@ -4,27 +4,43 @@ import pickle
 
 import FeatureSpace as fs
 
-threshold = 3.
+which_exp = 'nveMem'
+Y = pickle.load(open('../out/{0}/Y.pk'.format(which_exp), 'r'))
 
-def classify_by_arrest_coeff(dist):
-    predictions = [int(x>threshold) for x in dist]
-    return predictions
+def classify_by_arrest_coeff(dist,threshold):
+    return [int(np.linalg.norm(x)>threshold) for x in dist]
 
 feat_space = fs.FeatureSpace()
 
 from get_features import data_panel, good_tracks
 
-predict_dict = {}
 
-i = 0
-for track in good_tracks:
-    pos = np.array([data_panel[track].dropna(axis=0)['x'], \
-                    data_panel[track].dropna(axis=0)['y']])
-    pos = np.transpose(pos)
-    track_length = pos.shape[0]
+def test_arrest(threshold):
+    predict_dict = {}
+
+    i = 0
+    for track in good_tracks:
+	pos = np.array([data_panel[track].dropna(axis=0)['x'], \
+			data_panel[track].dropna(axis=0)['y']])
+	pos = np.transpose(pos)
+	track_length = pos.shape[0]
     
-    steps = feat_space.get_steps(pos)
-    disp = np.zeros(track_length-1)
+	steps = feat_space.get_steps(pos,len(pos))
+	disp = np.zeros(track_length-1)
 
-    #predict_dict[i] = classify_by_arrest_coeff(steps)
-    i+=1
+	predict_dict[i] = classify_by_arrest_coeff(steps,threshold)
+	i+=1
+
+    predict_array = np.array([666])
+    for track in predict_dict.items():
+	for prediction in track:
+	    predict_array = np.hstack([predict_array,prediction])
+
+    predict_array = predict_array[1:]
+
+    perf =  np.sum([int(x) for x in (predict_array == Y['labels'].__array__())])/float(len(predict_array))
+    
+    return perf
+
+for thresh in np.linspace(0,2,10):
+    print test_arrest(thresh)
